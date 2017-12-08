07a9fb1d6c6e20603f5169a121bff7af6a6bf41f
Geoffrey Lebbos <lebbos.geoffrey@gmail.com>
2017-12-07 21:39:01 -0800

    Implemented toDisk()

--- src/Inode.java
+++ src/Inode.java
@@ -43,7 +43,30 @@ public class Inode {
     }
 
     int toDisk( short iNumber ) {                  // save to disk as the i-th inode
+        // save to disk as the i-th inode
         // design it by yourself.
-        return 0;
+        int offset = 0;
+        int blockOffSet = (iNumber % 16) *iNodeSize; //Location of inode in block
+        int blockNum = 1 + iNumber / 16; //Location of block on disk
+        byte[] nodeData = new byte[iNodeSize]; //32 Bytes in an inode, 16 inodes per block
+        byte[] blockData = new byte[Disk.blockSize]; //For reading from disk
+
+        SysLib.int2bytes(length, nodeData, offset); //Store length
+        offset += 4; //4 bytes per int
+        SysLib.short2bytes(count, nodeData, offset); //Store count
+        offset += 2; //2 bytes for short
+        SysLib.short2bytes(flag, nodeData, offset); //Store flag
+        offset += 2; //2 bytes for short
+
+        for(int i = 0; i < directSize; i++)  //Start writing direct pointers
+        {
+            SysLib.short2bytes(direct[i],nodeData, offset); //Convert short entries to bytes, store with offset
+            offset += 2; //Each short entry is 2 bytes
+        }
+        SysLib.short2bytes(indirect, nodeData, offset); //Write indirect block
+        SysLib.rawread(blockNum,blockData); //Read old data from disk
+        System.arraycopy(nodeData, 0, blockData, blockOffSet, iNodeSize);
+        SysLib.rawwrite(blockNum, blockData); //Write to disk
+        return 0; 
     }
 }
