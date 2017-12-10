081c360799e532b9d7cd451548c582c256871fc0
boggiex17 <bodik95@gmail.com>
2017-12-09 19:25:11 -0800

    Update FileSystem.java

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -197,284 +197,71 @@ public class FileSystem {
         return -1;
     }
 
+    // deallocAllBlock
+    // takes in a file table entry and goes through all the pointers deallocating the blocks
+    // return true or false based on successful execution
     private boolean deallocAllBlocks(FileTableEntry ftEnt) {
-        return false;
-    }
-
-    boolean delete(String filename) {
-        return false;
-    }
-
-    private final int SEEK_SET = 0;
-    private final int SEEK_CUR = 1;
-    private final int SEEK_END = 2;
-
-    int seek(FileTableEntry ftEnt, int offset, int whence) {
-        return 0;
-    }
-
-}
-
-
-/*
-public class FileSystem {
-    private SuperBlock sb;
-    private Directory dir;
-    private FileTable ftb;
-    private final int SEEK_SET = 0;
-    private final int SEEK_CUR = 1;
-    private final int SEEK_END = 2;
-
-    // CONSTRUCTOR
-    // takes in a int as a variable which is the disk size and uses it to initialize
-    // superblock, directory and Filetable
-    public FileSystem(int var1) {
-        sb = new SuperBlock(var1);
-        dir = new Directory(sb.totalInodes);
-        ftb = new FileTable(dir);
-        FileTableEntry var2 = open("/", "r");
-        int var3 = fsize(var2);
-        if(var3 > 0) {
-            byte[] var4 = new byte[var3];
-            read(var2, var4);
-            dir.bytes2directory(var4);
+        if (ftEnt.inode.count != 1)                 // if not valid
+            return  false;
+        for (int i = 0; i < 11; i++) {              // loop indirect block pointers
+            if (ftEnt.inode.direct[i] == -1)        // if pointing to nothing
+                continue;
+            else {                                  // pointing to data
+                superblock.returnBlock(i);          // get superblock
+                ftEnt.inode.direct[i] = -1;         // set pointer to nothing
+            }
         }
 
-        close(var2);
-    }
+        byte[] data = ftEnt.inode.unregisterIndexBlock(); // temporary read data
 
-    // SYNC
-    // Syncs FilTableEntry to disk
-    // and closes the FTE
-    // calls superblocks sync method
-
-    void sync() {
-        FileTableEntry var1 = open("/", "w");
-        byte[] var2 = dir.directory2bytes();
-        write(var1, var2);
-        close(var1);
-        sb.sync();
-    }
-
-    // FORMAT
-    // Calls superblocks format method
-    // and assigns dir and ftb variables new instances of their objects
-    boolean format(int var1) {
-        while(!ftb.fempty()) {
-            ;
+        if (data != null) {                         // data exists
+            short index;
+            while((index = SysLib.bytes2short(data, 0)) != -1) {    //
+                superblock.returnBlock(index);
+            }
         }
-
-        sb.format(var1);
-        dir = new Directory(sb.totalInodes);
-        ftb = new FileTable(dir);
         return true;
     }
 
-    // OPEN
-    // creates a FileTable Entry
-    // returns FileTable Entry
-    FileTableEntry open(String var1, String var2) {
-        FileTableEntry var3 = ftb.falloc(var1, var2);
-        return var2 == "w" && !deallocAllBlocks(var3)?null:var3;
-    }
+    // delete
+    // takes in a filename and deletes that file name from the system
+    boolean delete(String filename) {
+        FileTableEntry Entry = open(filename, "w");      // check if file exists
+        //short location = Entry.iNumber;
+        if (directory.ifree(Entry.iNumber))                     // remove from dirrectory
+            if (close(Entry))                                   // close
+                return true;                                    // success
 
-    // CLOSE
-    // calls FileTable's ffree method
-    boolean close(FileTableEntry var1) {
-        synchronized(var1) {
-            --var1.count;
-            if(var1.count > 0) {
-                return true;
-            }
+        return false;
+    }
+    private final int SEEK_SET = 0;
+    private final int SEEK_CUR = 1;
+    private final int SEEK_END = 2;
+
+    // seek
+    // takes in 3 arguments, a file table entry and offset int and an location int
+    // and used these arguments to set a new seek pointer to the entry that is
+    // passed in. Returns -1 if failed or the new seek pointer if success
+    int seek(FileTableEntry ftEnt, int offset, int whence) {
+
+        if(ftEnt != null) {          // if valid entry and offset
+            if (offset < 0)          // if offset is invalid make it valid
+                offset = 0;
+
+            if (whence == SEEK_SET) {           // if at beggining
+                ftEnt.seekPtr = offset;         // set pointer
+            } else if (whence == SEEK_CUR) {    // if at current
+                int newOffset = ftEnt.seekPtr + offset; // make new pointer
+                ftEnt.seekPtr = newOffset;              // set pointer
+            } else if (whence == SEEK_END) {    // if at end
+                int newOffset = ftEnt.inode.length + offset; // make new pointer
+                ftEnt.seekPtr = newOffset;                   // set pointer
+            } else
+                return -1;                      // not a case
+            return  ftEnt.seekPtr;              // return seek pointer
         }
-
-        return ftb.ffree(var1);
-    }
-
-    // FSIZE
-    // Returns length of file
-    int fsize(FileTableEntry var1) {
-        synchronized(var1) {
-            return var1.inode.length;
-        }
-    }
-
-    // READ
-    // Reads the file and returns an int depending on success of the method
-    int read(FileTableEntry var1, byte[] var2) {
-        if(var1.mode != "w" && var1.mode != "a") {
-            int var3 = 0;
-            int var4 = var2.length;
-            synchronized(var1) {
-                while(var4 > 0 && var1.seekPtr < fsize(var1)) {
-                    int var6 = var1.inode.findTargetBlock(var1.seekPtr);
-                    if(var6 == -1) {
-                        break;
-                    }
-
-                    byte[] var7 = new byte[512];
-                    SysLib.rawread(var6, var7);
-                    int var8 = var1.seekPtr % 512;
-                    int var9 = 512 - var8;
-                    int var10 = fsize(var1) - var1.seekPtr;
-                    int var11 = Math.min(Math.min(var9, var4), var10);
-                    System.arraycopy(var7, var8, var2, var3, var11);
-                    var1.seekPtr += var11;
-                    var3 += var11;
-                    var4 -= var11;
-                }
-
-                return var3;
-            }
-        } else {
-            return -1;
-        }
-    }
-
-    // WRITE
-    // writes to disk
-    // Parametes given a FileTableEntry and a byteArray to write to
-    // return an int corresponding on the status of the method at the end
-    int write(FileTableEntry var1, byte[] var2) {
-        if(var1.mode == "r") {
-            return -1;
-        } else {
-            synchronized(var1) {
-                int var4 = 0;
-                int var5 = var2.length;
-
-                while(var5 > 0) {
-                    int var6 = var1.inode.findTargetBlock(var1.seekPtr);
-                    if(var6 == -1) {
-                        short var7 = (short)sb.getFreeBlock();
-                        switch(var1.inode.registerTargetBlock(var1.seekPtr, var7)) {
-                            case -3:
-                                short var8 = (short)sb.getFreeBlock();
-                                if(!var1.inode.registerIndexBlock(var8)) {
-                                    SysLib.cerr("ThreadOS: panic on write\n");
-                                    return -1;
-                                }
-
-                                if(var1.inode.registerTargetBlock(var1.seekPtr, var7) != 0) {
-                                    SysLib.cerr("ThreadOS: panic on write\n");
-                                    return -1;
-                                }
-                            case 0:
-                            default:
-                                var6 = var7;
-                                break;
-                            case -2:
-                            case -1:
-                                SysLib.cerr("ThreadOS: filesystem panic on write\n");
-                                return -1;
-                        }
-                    }
-
-                    byte[] var13 = new byte[512];
-                    if(SysLib.rawread(var6, var13) == -1) {
-                        System.exit(2);
-                    }
-
-                    int var14 = var1.seekPtr % 512;
-                    int var9 = 512 - var14;
-                    int var10 = Math.min(var9, var5);
-                    System.arraycopy(var2, var4, var13, var14, var10);
-                    SysLib.rawwrite(var6, var13);
-                    var1.seekPtr += var10;
-                    var4 += var10;
-                    var5 -= var10;
-                    if(var1.seekPtr > var1.inode.length) {
-                        var1.inode.length = var1.seekPtr;
-                    }
-                }
-
-                var1.inode.toDisk(var1.iNumber);
-                return var4;
-            }
-        }
-    }
-
-
-    // DEALLOCALLBLOCKS
-    // Goes though all of the blocks and deallocates them
-    // returns a boolean true if success false otherwise
-    private boolean deallocAllBlocks(FileTableEntry var1) {
-        if(var1.inode.count != 1) {
-            return false;
-        } else {
-            byte[] var2 = var1.inode.unregisterIndexBlock();
-            if(var2 != null) {
-                byte var3 = 0;
-
-                short var4;
-                while((var4 = SysLib.bytes2short(var2, var3)) != -1) {
-                    sb.returnBlock(var4);
-                }
-            }
-
-            int var5 = 0;
-
-            while(true) {
-                Inode var10001 = var1.inode;
-                if(var5 >= 11) {
-                    var1.inode.toDisk(var1.iNumber);
-                    return true;
-                }
-
-                if(var1.inode.direct[var5] != -1) {
-                    sb.returnBlock(var1.inode.direct[var5]);
-                    var1.inode.direct[var5] = -1;
-                }
-
-                ++var5;
-            }
-        }
-    }
-
-    // DELETE
-    // Calls directory's ifree method to delete the file
-    // returns a boolean depending on the success of the method
-    boolean delete(String var1) {
-        FileTableEntry var2 = open(var1, "w");
-        short var3 = var2.iNumber;
-        return close(var2) && dir.ifree(var3);
-    }
-
-    // SEEK
-    // Seek finds the location in the file
-    // returns an int where the seekPtr is looking at
-    int seek(FileTableEntry var1, int var2, int var3) {
-        synchronized(var1) {
-            switch(var3) {
-                case 0:
-                    if(var2 >= 0 && var2 <= fsize(var1)) {
-                        var1.seekPtr = var2;
-                        break;
-                    }
-
-                    return -1;
-                case 1:
-                    if(var1.seekPtr + var2 >= 0 && var1.seekPtr + var2 <= fsize(var1)) {
-                        var1.seekPtr += var2;
-                        break;
-                    }
-
-                    return -1;
-                case 2:
-                    if(fsize(var1) + var2 < 0 || fsize(var1) + var2 > fsize(var1)) {
-                        return -1;
-                    }
-
-                    var1.seekPtr = fsize(var1) + var2;
-            }
-
-            return var1.seekPtr;
-        }
+        return -1;                              // error
     }
 }
-*/
-
-
-
 
 
